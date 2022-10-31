b86f50e59b54b1590c38594788582e9dd59453f9
Siddharth-Hanchinal <Siddharth05hanchinal@gmail.com>
2022-10-31 21:32:17 +0530

    varibale declared

---
+++ 04-vars.sh
@@ -0,0 +1,15 @@
+#!/bin/bash  
+
+a=10 
+# a is 10 and is integer 
+
+b=abc 
+# abc is a string  
+
+d=150
+
+echo value of a is : $a 
+echo ${a}
+echo the meaning of the b is "${b}"
+
+echo value of d is : $d
