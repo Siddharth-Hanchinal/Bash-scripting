ddf190647cacac3650e5de1500a292988b13e6f0
Siddharth-Hanchinal <Siddharth05hanchinal@gmail.com>
2022-11-03 20:49:06 +0530

    special

--- 05-specialvars.sh
+++ 05-specialvars.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
- $0
- $1 to $9
- $*
- $#
- $$
+#  Here are the special variables 
+
+#  $0          : This will gives the name of the script you're running 
+#  $1 to $9    :You can pass a maximum of 9 variables from the command line when you're running the script 
+#  $*          : Prints you all the supplied variables in the script 
+#  $@          : Prints you all the supplied variables in the script 
+#  $#          : Prints you the number of variables
+#  $$          : Process ID of the script that you're running 
+#  $?          : Gives you the exit code of the previous comman
+
 
 echo script name your running is $0
 
@@ -18,6 +23,12 @@ echo value of b is $b
 echo value of c is $c 
 echo value of d is $d
 
+# sh scriptname.sh 100 200 300 
+# sh scriptname.sh  $1  $2  $3
 
+echo "Printing the vairables that we have passed in the script $*"
+echo "Printing the vairables that we have passed in the script $@"
+echo "Printing the number vairables that we have passed in the scrip $#"
+echo "Prining the PID of the script that we are running $$"
 
 
